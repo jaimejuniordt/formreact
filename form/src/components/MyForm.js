affa6f58b26a104ea5ff07dafb03acde4d34276d
jaimejuniordt <jaimejuniordt@gmail.com>
2023-05-15 00:19:03 -0300

    formularioreact2
    Gerenciamento de dados de input

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -1,13 +1,24 @@
 import './MyForm.css'
 
+import {useState} from 'react'
 const MyForm = () => {
+  // 3- gerenciamento de dados
+  const [name, setName] = useState()
+  const [email, setEmail] = useState ()
+
+  const handleName = (e) => {
+    console.log(e.target.value);
+  };
+
+  console.log(name);
+
   return (
     <div>
       {/* 1 - criação de form */}
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite o seu nome" />
+          <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} />
         </div>
         {/* 2 - label envolvendo input - maneira mais simplificada do que a feita na 1 - acima*/}
         <label>
